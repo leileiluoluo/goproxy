453daba3d6479b94647025b13a63eb600fe102df
olzhy <olzhy@qq.com>
2019-06-22 11:05:37 +0800

    path validation test

--- pkg/proxy/proxy_test.go
+++ pkg/proxy/proxy_test.go
@@ -2,10 +2,30 @@ package proxy
 
 import "testing"
 
+func TestPathValidation(t *testing.T) {
+	paths := map[string]bool{
+		"rsc.io/@latest":          false,
+		"rsc.io/quote":            false,
+		"github.com/olzhy/quote":  false,
+		"rsc.io/quote/@v/@latest": true,
+	}
+	for path, valid := range paths {
+		err := pathValidation(path)
+		if valid && nil != err {
+			t.Errorf("path valid, but got a err, path: %s", path)
+			return
+		}
+		if !valid && nil == err {
+			t.Errorf("path invalid, but has no err, path: %s", path)
+			return
+		}
+	}
+}
+
 func TestPathRevision(t *testing.T) {
 	paths := []struct {
-		path  string
-		after string
+		before string
+		after  string
 	}{
 		{
 			"gopkg.in/%21data%21dog/dd-trace-go.v1/@v/v1.10.0.info",
@@ -17,7 +37,7 @@ func TestPathRevision(t *testing.T) {
 		},
 	}
 	for _, path := range paths {
-		rev := pathRevision(path.path)
+		rev := pathRevision(path.before)
 		if path.after != rev {
 			t.Errorf("want: %s, got: %s", path.after, rev)
 		}
